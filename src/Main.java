0932d9f7fac7f2e84827a244f99b0ce276c91843
jkLm972 <lucas.leotin@efrei.net>
2022-12-03 18:08:03 +0100

    dfddhhhhh

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Enter 1 to see the list of employees and enter 2 if you want to add a employee");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Veuillez saisir un mot :");
+        String str = sc.nextLine();
+        System.out.println("Vous avez saisi : " + str);
+
+
+
+
+        }
     }
 }
--- src/gggg.java
+++ src/gggg.java
@@ -1,3 +1,2 @@
 public class gggg {
-erthgzrth
 }
