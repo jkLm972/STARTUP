76a889f6f3d13296ec6ce29c9365119cade2aea4
Fiaansoo <94602773+Fiaansoo@users.noreply.github.com>
2022-12-05 10:43:17 +0100

    Update Contact.java

--- src/Contact.java
+++ src/Contact.java
@@ -2,4 +2,28 @@ public class Contact {
     private String Phone;
     private String Email;
     private int HomeNumber;
+    
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public int getHomeNumber() {
+        return HomeNumber;
+    }
+
+    public void setHomeNumber(int homeNumber) {
+        HomeNumber = homeNumber;
+    }
 }
