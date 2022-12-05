708023b0b4610ba0324c3fc3d7c3d18f2b17fcc0
Fiaansoo <94602773+Fiaansoo@users.noreply.github.com>
2022-12-05 10:44:27 +0100

    Update Position.java

--- src/Position.java
+++ src/Position.java
@@ -1,4 +1,21 @@
 public class Position {
     String Code;
     String Statut;
+    
+    public String getCode() {
+        return Code;
+    }
+
+    public void setCode(String code) {
+        Code = code;
+    }
+
+    public String getStatut() {
+        return Statut;
+    }
+
+    public void setStatut(String statut) {
+        Statut = statut;
+    }
+}
 }
