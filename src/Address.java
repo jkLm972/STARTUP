98240180ac12fcdcafa89bec633ca82ef3ceaeb4
Fiaansoo <94602773+Fiaansoo@users.noreply.github.com>
2022-12-05 10:47:28 +0100

    Update Address.java

--- src/Address.java
+++ src/Address.java
@@ -1,4 +1,20 @@
 public class Address {
     private String StreetAddress;
     private String  PostalAddress;
+    
+    public String getStreetAddress() {
+        return StreetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        StreetAddress = streetAddress;
+    }
+
+    public String getPostalAddress() {
+        return PostalAddress;
+    }
+
+    public void setPostalAddress(String postalAddress) {
+        PostalAddress = postalAddress;
+    }
 }
