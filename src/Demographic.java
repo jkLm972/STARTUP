8861f77076453e2402f88bbf6af05b8ca211eeea
Fiaansoo <94602773+Fiaansoo@users.noreply.github.com>
2022-12-05 10:41:09 +0100

    Update Demographic.java

--- src/Demographic.java
+++ src/Demographic.java
@@ -1,7 +1,21 @@
 public class Demographic {
     private String Gender;
     private String Race;
+    
+    public String getGender() {
+        return Gender;
+    }
 
+    public void setGender(String gender) {
+        Gender = gender;
+    }
 
+    public String getRace() {
+        return Race;
+    }
 
+    public void setRace(String race) {
+        Race = race;
+    }
+}
 }
