fc5d67a9d9ecd0638fc0935a85371dc4e153a5f4
Fiaansoo <94602773+Fiaansoo@users.noreply.github.com>
2022-12-11 21:57:57 +0100

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
         //domain = put all the classes and entities
         //factory = objects that you create for your entities
         //repository = where you keep your data stored
