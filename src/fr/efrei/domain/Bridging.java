9c5e75909c108050148f0e2a6a4d4b973402e0ea
Lucas <81096649+jkLm972@users.noreply.github.com>
2022-12-11 21:26:17 +0100

    Add files via upload
    Class and Interface from Samy, Sofiane and Lucas

---
+++ src/fr/efrei/domain/Bridging.java
@@ -0,0 +1,13 @@
+package fr.efrei.domain;
+
+public class Bridging {
+    private String Address;
+    private String Gender;
+    private String Demographic;
+    private String Race;
+    private String Contact;
+    private String Position;
+    private String Job;
+    private String Salary;
+    private String Identity;
+}
